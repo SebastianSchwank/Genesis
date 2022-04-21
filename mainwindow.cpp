2d37bdeac85034c43c92f422c4a2285272520b90
Sebastian Schwank <sebastian.schwank@googlemail.com>
2022-04-21 16:06:54 +0200

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -163,7 +163,7 @@ void MainWindow::processNet(){
                    Cluster0->resetSampler(false);
                    for(int i = 0; i < 32; i++){
                        Cluster0->propergate(inputV,targetV,false,false,false);
-                       Cluster0->train(0.1);
+                       Cluster0->train(0.01);
 
                    }
                    out0 = Cluster0->getTarget();
