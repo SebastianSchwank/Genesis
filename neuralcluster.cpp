57985ac0ec335ff0b328cbd363a9d4be40cae3da
Sebastian Schwank <sebastian.schwank@googlemail.com>
2023-04-27 16:19:40 +0200

    yet anothe stability update

--- neuralcluster.cpp
+++ neuralcluster.cpp
@@ -311,10 +311,10 @@ void NeuralCluster::applyLearning(float learningRate,float globalRMSError, int t
 
 
             if(type == synapseType[j][i]||type == synapseType[i][j] || synapseType[i][j] == -1 || synapseType[j][i] == -1) weightsActive[i][j] -= (1.0-lastReal[j])*activationI*(meanInputSignal/weightsActive.size())*learningRate*globalRMSError;
-            if(type == synapseType[j][i]||type == synapseType[i][j] || synapseType[i][j] == -1 || synapseType[j][i] == -1) weightsActive[j][i] -= ((1.0-activationI)*activationI*(lastReal[j])*meanOutputInactive/weightsActive.size())*learningRate*globalRMSError;
-            if(type == synapseType[j][i]||type == synapseType[i][j] || synapseType[i][j] == -1 || synapseType[j][i] == -1) weightsActive[i][j] -= (1.0-activationI)*(abs(lastReal[j]-lastReal[i])*(2.0*rand()/RAND_MAX-1.0)*0.001)*learningRate*learningRate;
+            if(type == synapseType[j][i]||type == synapseType[i][j] || synapseType[i][j] == -1 || synapseType[j][i] == -1) weightsActive[j][i] -= (activationI*(lastReal[j])*meanOutputInactive/weightsActive.size())*learningRate*globalRMSError;
+            if(type == synapseType[j][i]||type == synapseType[i][j] || synapseType[i][j] == -1 || synapseType[j][i] == -1) weightsActive[i][j] -= activationI*(1.0-activationI)*((1.0-abs(activationI-lastReal[i]))*(2.0*rand()/RAND_MAX-1.0))*learningRate;
         }
-        weightsActive[i][weightsActive.size()-1] -= (meanInputSignal/weightsActive.size()+2.0*weightsActive[i][weightsActive.size()-1])*learningRate;
+        weightsActive[i][weightsActive.size()-1] -= (meanInputSignal/weightsActive.size()+1.0*weightsActive[i][weightsActive.size()-1])*learningRate;
     }
 
     float sumAbsWeights = 0.0;
